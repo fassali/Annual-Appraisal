5674560412a05ba169ef18aaff0f6614e11c0053
akchoukri <achoukri@ymagisgroup.com>
2018-10-29 14:47:36 +0000

    modif

--- src/main/java/com/ymagis/appraisal/AppraisalApplication.java
+++ src/main/java/com/ymagis/appraisal/AppraisalApplication.java
@@ -1,12 +1,30 @@
 package com.ymagis.appraisal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AppraisalApplication {
+import com.ymagis.appraisal.dao.SkilsRepository;
+import com.ymagis.appraisal.model.MeaningSkils;
+import com.ymagis.appraisal.model.Skils;
 
+@SpringBootApplication
+public class AppraisalApplication implements CommandLineRunner{
+
+	@Autowired
+	private SkilsRepository skilsRepository;
     public static void main(String[] args) {
         SpringApplication.run(AppraisalApplication.class, args);
     }
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+//		Skils skils = new Skils("tesst");
+//		MeaningSkils meaningSkils = new MeaningSkils(1, "desciption test");
+//		meaningSkils.setSkil(skils);
+//		skilsRepository.save(skils);
+//		skilsRepository.save(new Skils("Team Spirit"));
+	}
 }
