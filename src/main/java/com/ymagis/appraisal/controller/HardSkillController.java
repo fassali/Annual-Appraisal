203aaa01c6d60cddf059880920ce703e5117cf2d
ABE <abenabdellouahad-nyg@ymagisgroup.com>
2018-11-12 14:27:37 +0000

    competencies

--- src/main/java/com/ymagis/appraisal/controller/HardSkillController.java
+++ src/main/java/com/ymagis/appraisal/controller/HardSkillController.java
@@ -1,42 +1,63 @@
 package com.ymagis.appraisal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ymagis.appraisal.entities.ApHardSkill;
 import com.ymagis.appraisal.repository.HardSkillRepository;
 
 @RestController
-@CrossOrigin
+@RequestMapping(value = "/competencies", produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 public class HardSkillController {
 
 	@Autowired
-	private HardSkillRepository hardSkillRepository;
+	private HardSkillRepository repository;
 
-	@GetMapping(value = "/competencies")
-	public List<ApHardSkill> getHardSkills(@RequestParam("idApEmp") Long idApEmp) {
-		return hardSkillRepository.namedFindAllApHardSkillsByApEmploye(idApEmp);
-		
+	@RequestMapping(value = "/appraisal/{id}", method = RequestMethod.GET)
+	List<ApHardSkill> getByAppraisal(@PathVariable("id") Long id) {
+		return repository.namedFindAllApHardSkillsByApEmploye(id);
 	}
-	
-	@RequestMapping(method = RequestMethod.POST, value = "/competencies/save")
-	public ApHardSkill saveApHardSkill(@RequestBody ApHardSkill hardSkill) {
-		hardSkillRepository.save(hardSkill);
-		return hardSkill;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	List<ApHardSkill> getAll() {
+		return repository.findAll();
 	}
-	
-	@RequestMapping(method = RequestMethod.PUT, value = "/competencies/update")
-	public ApHardSkill updateApHardSkill(@RequestBody ApHardSkill hardSkill) {
-		hardSkillRepository.save(hardSkill);
-		return hardSkill;
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public ApHardSkill save(@RequestBody ApHardSkill model) {
+		repository.save(model);
+		return model;
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	ApHardSkill get(@PathVariable(value = "id") Long id) throws Exception {
+		Optional<ApHardSkill> item = repository.findById(id);
+		if (item.isPresent()) {
+			return item.get();
+		}
+		return null;
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ApHardSkill update(@RequestBody ApHardSkill model, @PathVariable("id") Long id) {
+		model.setIdApHdSkill(id);
+		repository.save(model);
+		return model;
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable(value = "id") Long id) throws Exception {
+		repository.deleteById(id);
 	}
 
 }
--- src/main/resources/static/js/hardSkill/hardSkillService.js
+++ src/main/resources/static/js/hardSkill/hardSkillService.js
@@ -1,61 +1,33 @@
-var app = angular.module("myApp");
+app.service('hardSkillService', function($http) {
+	var that = this;
+	that.baseUrl = "/competencies";
 
-app.service("hardSkillService", function($http, $location) {
-
-	// appraisal competencies
-	this.getCompetencies = function(idApEmp) {
-		console.log(idApEmp);
-		var promise1 = $http({
-			method : "GET",
-			url : "http://localhost:8080/competencies?idApEmp=" + idApEmp
-		});
-
-		var promise2 = promise1.then(function mySuccess(response) {
-			return response.data;
-		}, function myError(error) {
-			console.log(error)
-		});
-		return promise2;
-	};
-
-	// ratings
-	this.getRatings = function() {
-		var promise1 = $http({
-			method : "GET",
-			url : "http://localhost:8080/Ratings"
-		});
-
-		var promise2 = promise1.then(function mySuccess(response) {
-			return response.data;
-			console.log(response.data);
-
-		}, function myError(error) {
-			console.log(error)
-		});
-		return promise2;
+	that.getAll = function() {
+		return $http.get(that.baseUrl + "/");
 	};
 	
-	// add competencies
-	this.addCompetencie = function(competency) {
-		console.log(competency);
-		return $http.post("http://localhost:8080/competencies/save",competency)
-        .then(function mySuccess(response) {
-        	return response.data;
-        }, function myError(response) {
-        	console.log(error.data);
-        });
+	that.getByAppraisal = function(id) {
+		return $http.get(that.baseUrl + "/appraisal/" + id);
 	};
 
-	// update competencies
-	this.updateCompetencies = function(competencies) {
-		console.log(competencies);
-		return $http.put("http://localhost:8080/competencies/", competencies)
-				.then(function mySuccess(response) {
-					return response.data;
-				}, function myError(error) {
-					console.log(error.data);
-				});
-
+	that.get = function(id) {
+		return $http.get(that.baseUrl + "/" + id);
 	};
 
-})
+	that.update = function(id, model) {
+		return $http.put(that.baseUrl + "/" + id, model);
+	};
+
+	that.remove = function(id) {
+		return $http.delete(that.baseUrl + "/" + id);
+	};
+
+	that.save = function(model) {
+		return $http.post(that.baseUrl + "/save", model);
+	};
+	
+	that.getRatings = function() {
+		return $http.get("/Ratings");
+	};
+});
+
